c212c00ff13b329479946949cc417d258aff9135
Virgilio Santos <virgilio.santos@rocketwave.tech>
2023-08-31 18:04:59 -0300

    first commit

---
+++ boletos/src/test/java/dev/vergil/boletos/BoletosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vergil.boletos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoletosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
